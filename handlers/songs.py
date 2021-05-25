8e2af2aa831fb4b9d6be81ecddbe95104f5c4d7e
ophuok <84042863+ophuok@users.noreply.github.com>
2021-05-25 10:00:02 +0000

    Update songs.py
    Ho

--- handlers/songs.py
+++ handlers/songs.py
@@ -41,7 +41,7 @@ def song(client, message):
 
     except Exception as e:
         m.edit(
-            "❌ Found Nothing.\n\nTry another keywork or maybe spell it properly."
+            "❌ Found Nothing.Try another keywork or maybe spell it properly."
         )
         print(str(e))
         return
@@ -51,7 +51,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Enjoy your song**'
+        rep = '**🎵 Enjoy your song **'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
